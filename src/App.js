9b2e6c19d58ad0837683a9aa048caed3e06c3209
razikhaNawaz <razikhafaheem02@gmail.com>
2023-10-16 23:34:17 +0530

    headeradded

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 import Header from './Components/Header';
-import Sidebar from './Components/Sidebar';
+import UpcomingTasks from './Components/UpcomingTasks'; // Import UpcomingTasks component
+import CompletedTasks from './Components/CompletedTasks'; // Import IncomingTasks component
+import Sidebar from './Components/Sidebar'
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Sidebar />
+        <Routes>
+          <Route path="/UpcomingTasks" component={UpcomingTasks} />
+          <Route path="/CompletedTasks" component={<CompletedTasks />} />
+          {/* Define your other routes here */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
---
+++ src/Components/CompletedTasks.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IncomingTasks = () => {
+  return (
+    <div>Incoming</div>
+  )
+}
+
+export default IncomingTasks;
